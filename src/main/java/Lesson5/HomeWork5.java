29ec16ab6fc9c8fcdf04824594d40eb0edbd3ef5
Evgeniy16312 <84197411+Evgeniy16312@users.noreply.github.com>
2021-05-25 16:28:56 +0400

    делаю дз

---
+++ src/main/java/Lesson5/HomeWork5.java
@@ -0,0 +1,17 @@
+package Lesson5;
+
+public class HomeWork5 {
+
+
+    public static void main(String[] args) {
+
+
+        Assistant assistant1 = new Assistant("Петров Петр Петрович", "Инженер","@petrov.ru",89277333333l,45000,44);
+        System.out.println(assistant1);
+
+
+    }
+
+
+
+}
