38bb49d44dd7e0f372dac87c0a0ede6e0e6d76d5
Evgeniy16312 <turin.92@mail.ru>
2021-05-26 22:35:42 +0400

    дз 5 доделано

--- src/main/java/Lesson5/HomeWork5.java
+++ src/main/java/Lesson5/HomeWork5.java
@@ -5,10 +5,10 @@ public class HomeWork5 {
 
     public static void main(String[] args) {
 
-        Assistant assistant1 = new Assistant("Петров Петр Петрович", "Инженер", "@petrov.ru", 89277333333l, 45000, 44);
+        Assistant assistant1 = new Assistant("Петров Петр Петрович", "Инженер", "@petrov.ru", 89277333333l, 45000, 48);
         System.out.println("Сотрудник: " + Assistant.getAssistantCount());
         assistant1.print1();
-        Assistant assistant2 = new Assistant("Иванов Иван Иванович", "Технолог", "@ivanov.ru", 89277775547l, 30000, 34);
+        Assistant assistant2 = new Assistant("Иванов Иван Иванович", "Технолог", "@ivanov.ru", 89277775547l, 30000, 39);
         System.out.println("Сотрудник: " + Assistant.getAssistantCount());
         assistant2.print1();
         Assistant assistant3 = new Assistant("Сидоров Сидр Сидорович", "Проектант", "@sidorod.ru", 89278885544l, 35000, 36);
@@ -17,8 +17,8 @@ public class HomeWork5 {
         System.out.println("----------------------------------------------------------------------------------------------------------------------");
         Employee[] persArray = new Employee[5];
         persArray[0] = new Employee("Вячеслав Кобрин", "Генеральный директор", "vk@primer,ru", "+79996665544", 250000, 55);
-        persArray[1] = new Employee("Иван Зайцев", "Директор по развитию", "vz@primer,ru", "+79998884455", 127000, 42);
-        persArray[2] = new Employee("Дмитрий Медведев", "Охранник", "dm@primer,ru", "+79997771234", 19000, 21);
+        persArray[1] = new Employee("Иван Зайцев", "Директор по развитию", "vz@primer,ru", "+79998884455", 127000, 47);
+        persArray[2] = new Employee("Дмитрий Медведев", "Охранник", "dm@primer,ru", "+79997771234", 19000, 27);
         persArray[3] = new Employee("Олеся Лисина", "Ведущий разработчик", "ol@primer,ru", "+79995554789", 120000, 28);
         persArray[4] = new Employee("Кристина Волкова", "Директор по работе с клиентами", "kv@primer,ru", "+79994445896", 138000, 48);
         // если у объекта в ячейке i поле age > 40, то у него вызывается метод print()
